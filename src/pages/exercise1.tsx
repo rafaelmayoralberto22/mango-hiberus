d3bf9ef8f358e36da7427a4ec18ee3f1d375bd98
Rafael Mayor Alberto <rafaelmayor22@gmail.com>
2023-05-25 11:19:49 -0400

    init config project

---
+++ src/pages/exercise1.tsx
@@ -0,0 +1,9 @@
+const PageExecise1 = () => {
+    return (
+      <div>
+        <h1>Hello, world 1!</h1>
+      </div>
+    );
+  };
+  
+  export default PageExecise1;
---
+++ src/pages/exercise2.tsx
@@ -0,0 +1,9 @@
+const PageExecise2 = () => {
+    return (
+      <div>
+        <h1>Hello, world2!</h1>
+      </div>
+    );
+  };
+  
+  export default PageExecise2;
